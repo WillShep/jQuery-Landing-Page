df6bad228852a2e7d3acc56ecf60259599180d23
Will Sheppard <willsheppard@Wills-MacBook-Air.local>
2016-10-18 09:45:57 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	new WOW().init();
+	
+	$(window).on("scroll", function() {
+		var scrollDistance = $(window).scrollTop();
+	    if (scrollDistance > 50) {
+	    	$("#mainNav").removeClass("affix-top").addClass("affix");
+	    }
+	    else {
+	    	$("#mainNav").removeClass("affix").addClass("affix-top");
+	    }
+	});
+
+
+	// $(window).on("click", ".page-scroll", function() {
+	// 	$("body").animate({
+ //    		scrollTop: $("#about").offset().top
+	// });
+
+	$(document).on("click", ".page-scroll", function(){
+		var target= $(this).attr("href");
+	
+	$("body").animate({
+		scrollTop:$(target).offset().top
+	}, 2000);
+	});
+
+	$("body").scrollspy({ target: "#mainNav" });
+
+});
